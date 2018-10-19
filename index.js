efbe2aae33468e2b7be1b646b403c40d283f2160
ameba23 <ameba23@systemli.org>
2018-10-19 19:07:05 +0200

    got what i want

--- index.js
+++ index.js
@@ -8,37 +8,41 @@ const getContent = require('ssb-msg-content')
 var keys = require('ssb-keys')
   .loadOrCreateSync(path.join(config.path, 'secret'))
 
-var rootName, rootTime
+var name, created, roots = []
 
 ssbClient(keys, config, (err, sbot) => {
   if (err) throw err
   sbot.whoami((err, msg) => {
     if (err) throw err
-
+    const me = msg.id
     sbot.about.get((err, about) => {
       var darkCrystal = DarkCrystal(sbot)
       pull(
         darkCrystal.root.pull.mine({live: false}),
         pull.drain(root => {
-          rootName = getContent(root).name
-          rootTime = new Date(root.value.timestamp).toLocaleDateString()
-          console.log(rootName, rootTime)
-          pull(
-            darkCrystal.shard.pull.byRoot(root.key),
-            pull.collect((err,shards) => {
-              console.log(shards.map(s => getContent(s).recps));
-            })
-          )
+          roots.push(root)
         // console.log(JSON.stringify(root,null,4));
-        }, done(sbot))
+        }, () => {
+          roots.map(root => {
+            pull(
+              darkCrystal.shard.pull.byRoot(root.key),
+              pull.collect((err, shards) => {
+                var recps = shards.map(s => getContent(s).recps.filter(r => r !== me))
+                const names = recps.map(r => resolveId(r, about))
+                name = getContent(root).name
+                created = new Date(root.value.timestamp).toLocaleDateString()
+                
+                console.log(name, created, names )
+              })
+            )
+          })
+          sbot.close()
+        })
       )
     })
   })
 })
-function done (sbot) {
-  return () => { sbot.close() }
-}
 
 function resolveId (id, about) {
-  return about[id].name[id][0]
+  return Object.values(about[id].name).find(i => typeof i[0] === 'string')[0]
 }
